9c89ce9ae8c0e36c95f03940783f0a3e3a0fa124
Amer Kulovic <amer.kulovic@yahoo.com>
2023-01-18 11:22:56 -0600

    created viewFile function to ask user a question

---
+++ index.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+const express = require("express");
+
+function viewFile() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to view?",
+        name: "mainMenu",
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Role", "Add an Employee", "Update a Role"],
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+viewFile();
